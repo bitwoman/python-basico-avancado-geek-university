bdff337e0b2bf41167817234ffb4c5ef24cc90a1
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:13:55 -0300

    exercicio_18

--- exercicio_18.py
+++ exercicio_18.py
@@ -1,2 +1,8 @@
 #18. Leia um valor de volume em metros cúbicos m³ e apresente-o convertido em litros.
 #A fórmula de conversão é L = 1000 * M, sendo L o valor em litros e M o valor em metros cúbicos.
+
+const = 1000
+mc = float(input('Digite um valor em metros cúbicos: '))
+litros = const * mc
+
+print('O valor %.2f em metros cúbicos, equivale à %.2f em litros' %(mc, litros))
