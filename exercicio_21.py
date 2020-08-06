839c6d2c5398e99ff6d3ee416dd1cb5dbd20aecc
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:15:57 -0300

    exercicio_21

---
+++ exercicio_21.py
@@ -0,0 +1,8 @@
+#21. Leia um valor de massa em libras e apresente-o convertido em quilogramas.
+#A fórmula de conversão é K = L * 0.45, sendo L o valor em libras e K o valor em quilogramas.
+
+const = 0.45 
+libras = float(input('Digite um valor de massa em libras: '))
+quilogramas = litros*const
+
+print('O valor %.2f de massa em libras , equivale à %.2f em quilogramas.' %(libras, quilogramas))
