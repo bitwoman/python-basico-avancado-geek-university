02793b5d1cd61f0d34cd4574ec799ab0f8de2b3d
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:21:46 -0300

    exercicio_29

---
+++ exercicio_29.py
@@ -0,0 +1,10 @@
+#29. Leia quatro notas, calcule a média aritmética e imprima o resultado.
+
+nota_1 = float(input('Digite o valor da primeira nota: '))
+nota_2 = float(input('Digite o valor da segunda nota: '))
+nota_3 = float(input('Digite o valor da terceira nota: '))
+nota_4 = float(input('Digite o valor da quarta nota: '))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4)/4
+
+print(f'A média aritmética desses valores equivale à {media}.')
