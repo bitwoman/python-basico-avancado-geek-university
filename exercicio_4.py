8edaa43412db269db56ead7f64827df11a6b9399
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:03:31 -0300

    exercicio_4

---
+++ exercicio_4.py
@@ -0,0 +1,5 @@
+#4. Leia um número real e imprima o resultado do quadrado deste número.
+numero_real = int(input('Digite um número real qualquer: '))
+quadrado = numero_real**2
+
+print(f'O quadrado do {numero_real} é {quadrado}.')
