efc038682a17fcbed49ee11ffb3672d3124dcc12
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:04:07 -0300

    exercicio_5

---
+++ exercicio_5.py
@@ -0,0 +1,5 @@
+#5. Leia um número real e imprima a quinta parte deste número.
+numero_real = int(input('Digite um número real qualquer: '))
+quinta_parte = numero_real * (1/5)
+
+print(f'A quinta parte de {numero_real} equivale à {quinta_parte}.')
