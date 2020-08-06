96ac93b7a39a5a4181c99a7fdd80ae228038860e
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:02:53 -0300

    exercicio_3

---
+++ exercicio_3.py
@@ -0,0 +1,7 @@
+#3. Peça ao usuário para digitar três valores inteiros e imprima a soma deles.
+numero_1 = int(input('Digite um primeiro valor: '))
+numero_2 = int(input('Digite um segundo valor: '))
+numero_3 = int(input('Digite um terceiro valor: '))
+soma = numero_1 + numero_2 + numero_3
+
+print(f'A soma dos três valores correspondem à {soma}.')
