5ed6746d6c2326ee340c6926e7e07a601ef67069
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:23:30 -0300

    exercicio_33

---
+++ exercicio_33.py
@@ -0,0 +1,6 @@
+#33. Leia o tamanho do lado de um quadrado e imprima o resultado da sua área.
+
+lado_quadrado = int(input('Digite o tamanho do lado de um quadrado: '))
+area = lado_quadrado * lado_quadrado
+
+print(f'A área do quadrado equivale à: {area}.')
