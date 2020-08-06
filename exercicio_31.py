951709d2fd318a039282d394b3d71b3afdec0cc4
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-06 19:22:44 -0300

    exercicio_31

---
+++ exercicio_31.py
@@ -0,0 +1,7 @@
+#31. Leia um número inteiro e imprima o seu antecessor e o seu sucessor.
+
+numero = int(input('Digite um número inteiro: '))
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f'Antecessor: {antecessor}.\nSucessor: {sucessor}.')
