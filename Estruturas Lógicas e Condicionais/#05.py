44ef6900220113d094a5885eeda8c93612299801
Brenda <49522696+bitwoman@users.noreply.github.com>
2020-08-26 16:56:28 -0300

    #05

---
+++ Estruturas Lógicas e Condicionais/#05.py
@@ -0,0 +1,8 @@
+#5. Faça um programa que receba um número inteiro e verifique se este número é par ou ímpar.
+
+numero = int(input('Digite um número inteiro qualquer: '))
+
+if numero % 2 == 0:
+  print('O número é par!')
+else:
+  print('O número é ímpar!')
